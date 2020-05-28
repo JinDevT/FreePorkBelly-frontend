6a9bc817bc4626e3831f58f139fb16d9575d8b2e
kenneth8128 <kenkim@younggap.co>
2020-05-28 22:26:05 +0900

    wip

--- src/pages/List/List.js
+++ src/pages/List/List.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import Header from '../../components/Header/Header'
+// import Footer from '../../components/Footer/Footer'
+import ProductImages from "../../components/ProductImages/ProductImages";
 import './List.scss';
 
 class List extends Component {
     render() {
         return (
-            <div>List page</div>
+            <>
+            <Header />
+                <img className="bestFresh" src="https://www.jeongyookgak.com/assets/list/01.png"/>
+                <div className="menu">
+                    <div className="all">전체보기</div>
+                    <div className="pork">돼지고기</div>
+                    <div className="beef">소고기</div>
+                    <div className="chicken">닭고기</div>
+                    <div className="egg">달걀</div>
+                    <div className="milk">우유</div>
+                    <div className="baby">이유식</div>
+                </div>
+                <div className="showRoom">
+                <ProductImages />
+                <ProductImages />
+                <ProductImages />
+                <ProductImages />
+                <ProductImages />
+                <ProductImages />
+                </div>
+            {/* <Footer /> */}
+            </>
         );
     }
 }
