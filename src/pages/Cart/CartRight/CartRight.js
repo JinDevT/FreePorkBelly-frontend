c8f7a7e9ff89834b842b46f8b95cc9aa245e1350
Kjintae <wlsxo2112@naver.com>
2020-05-28 11:41:50 +0900

    [ADD] 장바구니 페이지 레이아웃 작업 완료. - 장바구니 레이아웃 작업 완료 - 컴포는트화 작업 완료. - 수량 및 가격 업데이트 기능 및 상품 리스트 삭제 기능 구현 예정

---
+++ src/pages/Cart/CartRight/CartRight.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import './CartRight.scss';
+
+class CartRight extends Component {
+    render() {
+        return (
+            <div className="CartRight">
+                <div className="rightInner">
+                    <div className="priceBox">
+                        <div className="rightFlex">
+                            <span>총 상품 금액</span>
+                            <span>가격원</span>
+                        </div>
+                    </div>
+                    <div className="deliverBox">
+                        <div className="rightFlex">
+                            <span>총 배송비</span>
+                            <span>0원</span>
+                        </div>
+                        <div className="rightFlex justifyBox basicOrder">
+                            <span>기본 배송비</span>
+                            <span>2500원</span>
+                        </div>
+                        <div className="rightFlex justifyBox firstBuy">
+                            <span>첫구매 무료배송</span>
+                            <span>-100%</span>
+                        </div>
+                    </div>
+                    <div className="resultBox">
+                        <div className="rightFlex benefit">
+                            <span className="">첫구매 무료배송 혜택이 적용되었습니다.</span>
+                        </div>
+                        <div className="rightFlex justifyBox predictPrice">
+                            <span>예상 결제 금액</span>
+                        </div>
+                        <div className="rightFlex justifyBox allPrice">   
+                            <span>총합금액</span>
+                        </div>
+                    </div>
+                    <div className="buttonBox">
+                        <div className="btnWrap">
+                            <button type="button" className="orderBtn">
+                                <span>전체상품 주문하기</span>
+                            </button>
+                        </div>
+                        <div className="btnWrap">
+                            <button type="button" className="shoppingBtn">
+                                <span>쇼핑계속하기</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}   
+
+export default CartRight;
