11c8dc7f9dfd7bd3d2fd4faae4e9b49e3346e335
kenneth8128 <kenkim@younggap.co>
2020-05-29 22:27:51 +0900

    list 페이지 백엔드  data GET 및 Map함수 구현

--- src/pages/List/List.js
+++ src/pages/List/List.js
@@ -1,33 +1,62 @@
 import React, { Component } from 'react';
-import ProductImages from "../../components/ProductImages/ProductImages";
+import ProductImages from "../List/ProductImages/ProductImages";
 import Header from "../../components/Header/Header"
 import Footer from "../../components/Footer/Footer"
 import './List.scss';
 
 class List extends Component {
+    state = {
+        productList: []
+    }
+
+    componentDidMount(){
+        fetch("http://10.58.4.231:8000/product/category/0", {
+            method: "GET", 
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => response.json())
+        .then(response => this.setState({
+            productList: response
+        }, () => console.log("response setState: ", this.state.productList)))
+    }
+
     render() {
+        const { productList } = this.state;
+        console.log("render: ", productList.products_info)
         return (
             <>
-            <Header />
-                <img className="bestFresh" src="https://www.jeongyookgak.com/assets/list/01.png"/>
-                <div className="menu">
-                    <div className="all">전체보기</div>
-                    <div className="pork">돼지고기</div>
-                    <div className="beef">소고기</div>
-                    <div className="chicken">닭고기</div>
-                    <div className="egg">달걀</div>
-                    <div className="milk">우유</div>
-                    <div className="baby">이유식</div>
-                </div>
-                <div className="showRoom">
-                <ProductImages />
-                <ProductImages />
-                <ProductImages />
-                <ProductImages />
-                <ProductImages />
-                <ProductImages />
-                </div>
-            {/* <Footer /> */}
+                <Header />
+                    <div>
+                        <img className="bestFresh" src="https://www.jeongyookgak.com/assets/list/01.png"/>
+                    </div>
+                    <div className="menu">
+                        <div className="all">전체보기</div>
+                        <div className="pork">돼지고기</div>
+                        <div className="beef">소고기</div>
+                        <div className="chicken">닭고기</div>
+                        <div className="egg">달걀</div>
+                        <div className="milk">우유</div>
+                        <div className="baby">이유식</div>
+                    </div>
+                    <div className="showRoom">
+                        {
+                            productList.products_info && productList.products_info.map((product, index) => {
+                                return (
+                                    <ProductImages 
+                                        key={index}
+                                        imgUrl={product.sub_img_url}
+                                        name={product.name}
+                                        salesPrice={product.sales_price_comment}
+                                        unitPrice={product.unit_price_comment}
+                                       
+                                    />
+                                );
+                            })
+                        }
+                    </div>
+                <Footer />
             </>
         );
     }
