6de217eeab4b5da7ba55bbe0242cbd7e4455e4e2
Kjintae <wlsxo2112@naver.com>
2020-05-30 14:42:41 +0900

    [Refactor] 장바구니  이전 코드로 리펙토링 - 프론트에서 수량 및 값 처리하는게 무리가 있어서 백엔드에서 장바구니 구현전까지 이전 코드로 변경 - 백엔드 구현 완료 후, 백엔드와 통신하여 다시 재작업 시작

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -8,84 +8,64 @@ class Cart extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            allPrice : 15200,
-            products : [
-                {
-                    id: 0,
-                    img: "https://www.jeongyookgak.com/assets/thumbnail/newcart/porkleg-fresh.jpg",
-                    name: "초신선 돼지 앞다리",
-                    purpose: "제육욕",
-                    standard: "100g 기준",
-                    number: 1,
-                    price: 8700
-                },
-                {
-                    id: 1,
-                    img: "https://www.jeongyookgak.com/assets/thumbnail/newcart/milk-fresh.jpg",
-                    name: "우유",
-                    purpose: "이유식",
-                    standard: "100ml 기준",
-                    number: 3,
-                    price: 3200
-                }
-            ]
+            number: 1,
+            allPrice : 13000,
+            price: 13000
         }
     }
 
-    handleDecrease = (id) => {
-        const { products } = this.state;
-        console.log("Cart에 넘어온 감소버튼 name: ", products[id].number);
-        if(products[id].number === 1) { 
+    // componentDidMount() {
+    //     fetch("/data/cart.json",  {
+    //         method: "GET",
+    //         headers: {
+    //             "Content-type": "application/json",
+    //         },
+    //     })
+    //     .then(res => res.json())
+    //     .then(res => this.setState({
+    //         products : res
+    //     }))
+    // }
+    
+    handleDecrease = () => {
+        const { number } = this.state;
+        console.log("Cart에 넘어온 감소버튼 name: ",number);
+        if(number === 1) { 
             return;
-        } else if(products[id].number > 1) {
+        } else if(number > 1) {
             this.setState({
-                number: products[id].number - 1,
+                number: number - 1,
             }, () => this.calc())
         }
     };
     
     // 스프레드 오퍼레이터 ...
-    handleIncrease = id => {
-        const { products } = this.state;
-        const find = products.find(product => product.id === id)
-        console.log(find)
+    handleIncrease = () => {
+        const { number } = this.state;
+        console.log("증가 눌러서 여기 들어왔어!!", number)
         this.setState({
-            number : find.number + 1
-        })
-
-        // this.setState({
-        //     number: products[id].number + 1,
-        // }, () => this.calc(id));
-       
-        // this.setState({
-        //     ...this.state,
-        //     number: products.push(
-        //       products[id].number + 1,
-        //     )
-        
-        // }, () => console.log("setState: ", this.state.products[id].number))
-
-        this.setState({
-            
-        })
+            number:number + 1
+        }, () => this.calc())
     }
 
     handleRemove = id => {
         const { products } = this.state;
-        this.setState({
-            products: products.filter(product => product.id !== id)
-        })
+            if(products){
+            this.setState({
+                products: products.filter(product => product.id !== id)
+            })
+        }
     }
 
-    calc = (id) => {
-        const { number, products } = this.state;
+    calc = ( ) => {
+        const { number, price } = this.state;
         this.setState({
-            allPrice : products[id].price * number
+            allPrice : price * number
         }, () => console.log("calc :", this.state.allPrice));
     }
 
     render() {
-        const { number, products, allPrice } = this.state;
+        const {  number, price, allPrice } = this.state;
         return (
             <div className="Cart">
                 <Header />
@@ -99,9 +79,9 @@ class Cart extends Component {
                             onDecrease={this.handleDecrease} 
                             onRemove={this.handleRemove}
                             number={number}
-                            products={products}
+                            price={price}
                         />
-                        <CartRight allPrice={allPrice} />
+                        <CartRight allPrice={allPrice}  />
                     </div>
                 </div>
                 <Footer />
