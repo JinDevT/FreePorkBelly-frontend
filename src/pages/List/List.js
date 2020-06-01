079b1e82de7c7864e01bfd645589415f605c670c
kenneth8128 <kenkim@younggap.co>
2020-06-01 12:28:06 +0900

    [ADD] 1. <Header/><Footer/>추가       2. 약관동의 이전으로 버튼 누를 시 메인화면 링크 연결

--- src/pages/List/List.js
+++ src/pages/List/List.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
-import Header from "../../components/Header/Header"
-import Footer from "../../components/Footer/Footer"
 import './List.scss';
-<<<<<<< HEAD
-=======
-
->>>>>>> master
 
 class List extends Component {
     render() {
         return (
             <>
             <div>List page</div>
-            <Header />
+
             <div></div>
-            <Footer />
+
             </>
         );
     }
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import InfoRegister from '../Register/InfoRegister/InfoRegister';
 import AgreeTerms from '../Register/AgreeTerms/AgreeTerms';
+import Header from "../../components/Header/Header";
+import Footer from '../../../src/components/Footer/Footer';
 import './Register.scss';
 
 class Register extends Component {
@@ -25,10 +27,12 @@ handleChange2 = () => {
         const { stage } = this.state;
             return (
             <>
+             <Header />
                 <div className="signin">회원가입</div>
                     <div>
                         {stage ? <AgreeTerms stageChange={this.handleChange1}/> : <InfoRegister stageChange={this.handleChange2}/>  }
                     </div>
+             <Footer />
             </>
         );
     }   
