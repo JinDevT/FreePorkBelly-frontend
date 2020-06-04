f8c44f7628cca573dc9614ce3de76817ebe26a62
Kjintae <wlsxo2112@naver.com>
2020-06-04 20:21:59 +0900

    [Fix] 구매하기 버튼 클릭시 400 에러 수정 - 버튼 수량 및 옵션을 정한 후 구매하기 버튼 눌렀을 시, 400에러 해결 완료 (백단에 값을 넘겨주지 않았음)
    [Refactor] API 변수화 처리
    - API주소를 변수화 처리하여 사용함
    
    [ADD] Query parameter 적용
    - 앞으로 리스트에서 선택한 상품의 디테일을 보여줘야 하기 때문에 Query parameter를 적용함.

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -7,6 +7,7 @@ import Explicate from './Explicate/Explicate';
 import ReviewCmt from './ReviewCmt/ReivewCmt';
 import ProductInfo from './ProductInfo/ProductInfo';
 import Footer from '../../components/Footer/Footer';
+import { API } from '../../config';
 import './Detail.scss';
 class Detail extends Component {
     constructor(props) {
@@ -16,33 +17,44 @@ class Detail extends Component {
         }
     }
 
+    componentDidMount() {
+        fetch(`${API}/product/${this.props.match.params.id}`,  {
+            method: "GET",
+            headers: {
+                "Content-type": "application/json",
+            },
+        })
+        .then(res => res.json()).catch(err => console.log("err: ", err));
+    }
+
     updateIndex = index => {
         this.setState({ index });
     }
     
     render() {
-        const { index } = this.state;
+        const { index, productInfo } = this.state;
+       
         return (
             <div className="Detail">
                 <Header />
-                <div className="container">
-                    <section className="cont1">
-                        <Product />
-                    </section>
-                    <section className="contBox cont2">
-                        <TabContainer index={index} updateIndex={this.updateIndex}>
-                            <Tab title="상품설명" className="listBox">
-                                <Explicate />
-                            </Tab>
-                            <Tab title="상품리뷰" className="listBox">
-                                <ReviewCmt />
-                            </Tab>
-                            <Tab title="상품정보안내" className="listBox">
-                                <ProductInfo />
-                            </Tab>
-                        </TabContainer> 
-                    </section>
-                </div>
+                    <div className="container">
+                        <section className="cont1">
+                            <Product productInfo={productInfo}/>
+                        </section>
+                        <section className="contBox cont2">
+                            <TabContainer index={index} updateIndex={this.updateIndex}>
+                                <Tab title="상품설명" className="listBox">
+                                    <Explicate />
+                                </Tab>
+                                <Tab title="상품리뷰" className="listBox">
+                                    <ReviewCmt />
+                                </Tab>
+                                <Tab title="상품정보안내" className="listBox">
+                                    <ProductInfo />
+                                </Tab>
+                            </TabContainer> 
+                        </section>
+                    </div>
                 <Footer />
             </div>
         );
--- src/pages/Detail/ReviewCmt/ReivewCmt.js
+++ src/pages/Detail/ReviewCmt/ReivewCmt.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Modal from '../Modal/Modal';
 import './ReviewCmt.scss';
 
+const body = document.getElementsByTagName('body')[0];
 class ReviewCmt extends Component {
     constructor(props) {
         super(props);
@@ -15,6 +16,7 @@ class ReviewCmt extends Component {
         this.setState({ 
             isModalShow: true
         })
+    body.classList.add("not_scroll");
     }
 
     // close modal
@@ -22,6 +24,7 @@ class ReviewCmt extends Component {
         this.setState({ 
             isModalShow: false
         })
+    body.classList.remove("not_scroll");
     }
 
     render() {
@@ -38,11 +41,11 @@ class ReviewCmt extends Component {
                             </div>
                             <div className="contentUser">
                                 <div className="userInfo">
-                                    <span className="userId">아이디</span>
-                                    <span className="buyNum">몇회구매</span>
+                                    <span className="userId">wecode</span>
+                                    <span className="buyNum">10회구매</span>
                                 </div>
                                 <div className="userDate">
-                                    <span className="buyDate">구매날짜</span>
+                                    <span className="buyDate">2020.06.04</span>
                                 </div>
                             </div>
                         </div>
@@ -52,6 +55,60 @@ class ReviewCmt extends Component {
                             </div>
                             <div className="togetherName">
                                 <span>초신선 돼지고기</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="contents">
+                        <div className="inner">
+                            <div className="contentTit">
+                                항상 이용하고있는 사이트
+                            </div>
+                            <div className="contentSubT">
+                                <span>만족한다.</span>
+                            </div>
+                            <div className="contentUser">
+                                <div className="userInfo">
+                                    <span className="userId">jintae</span>
+                                    <span className="buyNum">1회구매</span>
+                                </div>
+                                <div className="userDate">
+                                    <span className="buyDate">2020.06.01</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="togetherInfo">
+                            <div className="togetherTit">
+                                <span>함께 구매하신 상품</span>
+                            </div>
+                            <div className="togetherName">
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="contents">
+                        <div className="inner">
+                            <div className="contentTit">
+                                회식은 정육각에서!!
+                            </div>
+                            <div className="contentSubT">
+                                <span>회식으로 즐겨먹고 있습니다.</span>
+                            </div>
+                            <div className="contentUser">
+                                <div className="userInfo">
+                                    <span className="userId">freeForkBelly</span>
+                                    <span className="buyNum">8회구매</span>
+                                </div>
+                                <div className="userDate">
+                                    <span className="buyDate">2020.05.07</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="togetherInfo">
+                            <div className="togetherTit">
+                                <span>함께 구매하신 상품</span>
+                            </div>
+                            <div className="togetherName">
+                                <span>등심</span>
                             </div>
                         </div>
                     </div>
